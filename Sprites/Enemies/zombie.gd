1d0a52aa6a0297ce98b0ca49a9fa2e85a3314a03
Souuppp <levmcg78@kingshigh.school.nz>
2025-06-11 14:54:13 +1200

    yea

---
+++ Sprites/Enemies/zombie.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+var directon = 1
+
+func _ready():
+	pass
+
+
+func _process(delta):
+	translate(Vector2.RIGHT * direction)
+	
+
+
+func _on_timer_timeout() -> void:
+	direction *= -1 
