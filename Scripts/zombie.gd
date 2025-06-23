5ae6e418bcf70d8edc135e7954757afc5180539b
Souuppp <levmcg78@kingshigh.school.nz>
2025-06-24 09:27:20 +1200

    yea

--- Scripts/zombie.gd
+++ Scripts/zombie.gd
@@ -1,14 +1,19 @@
-extends Area2D
+extends CharacterBody2D
 
-var direction = 1
+@export var speed: float = 100.0
+@export var player_path: NodePath
 
-func _ready():
-	pass
+var player: Node2D
 
+func _ready() -> void:
+	if player_path != null and has_node(player_path):
+		player = get_node(player_path)
+	else:
+		push_warning("Player path is invalid or player node not found.")
 
-func _process(delta):
-	translate(Vector2.RIGHT * direction)
-	$Animatedsprite2d.flip_h = direction > 1
-
-func _on_timer_timeout():
-	direction *= -1 
+func _physics_process(_delta: float) -> void:
+	if player:
+		var direction = (player.global_position - global_position).normalized()
+		var movement = direction * speed
+		velocity = movement
+		move_and_slide()
