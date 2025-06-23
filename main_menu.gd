47d71acdc480c72c239868f1eda55c2605af0a1a
Matthewyee61 <matwat14@kingshigh.school.nz>
2025-06-24 10:00:26 +1200

    Version 1.1
    Coded main menu play button to world scene and exit to exit out of game.

--- main_menu.gd
+++ main_menu.gd
@@ -4,8 +4,8 @@ extends Control
 
 
 
-func _on_play_pressed() -> void:
-	pass # Replace with function body.
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://Scenes/world.tscn")
 
 
 func _on_options_pressed() -> void:
