4c52cb63dbcc684d4fb12ad6829ae9c120e8c013
Matthewyee61 <matwat14@kingshigh.school.nz>
2025-07-17 11:10:29 +1200

    Working on Pickup function and added animations

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,34 +1,83 @@
 extends CharacterBody2D
 
+const SPEED := 200.0
+const JUMP_FORCE := -350.0
+const GRAVITY := 1000.0
+const MAX_JUMPS := 2
 
-@export var speed = 300.0
-@export var jump_velocity = -400.0
-@export var acceleration : float = 15.0
-@export var jumps = 1
+var jumps_left := MAX_JUMPS
+var is_attacking := false
+var held_object: Node2D = null
 
-enum state {IDLE, RUNNING, JUMPUP, JUMPDOWN, HURT}
-
-var anim_state = state.IDLE
-
-@onready var animator = $AnimatedSprite2D
-@onready var animation_player = $AnimationPlayer
-
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var pickup_area: Area2D = $PickupArea
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_velocity
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = move_toward(velocity.x,direction*speed, acceleration)
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-
+	handle_gravity(delta)
+	handle_input()
 	move_and_slide()
+	handle_animation()
+
+func handle_input() -> void:
+	var direction := Input.get_action_strength("right") - Input.get_action_strength("left")
+
+	if not is_attacking:
+		velocity.x = direction * SPEED
+
+		if direction != 0:
+			sprite.flip_h = direction < 0
+
+		if Input.is_action_just_pressed("jump") and jumps_left > 0:
+			velocity.y = JUMP_FORCE
+			jumps_left -= 1
+
+		if Input.is_action_just_pressed("attack"):
+			attack()
+
+		if Input.is_action_just_pressed("pickup"):
+			pickup_or_drop()
+
+	if is_on_floor():
+		jumps_left = MAX_JUMPS
+
+func handle_gravity(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y += GRAVITY * delta
+	else:
+		velocity.y = 0
+
+func handle_animation() -> void:
+	if is_attacking:
+		return
+
+	if not is_on_floor():
+		if velocity.y < 0:
+			sprite.play("Jump_Up")
+		else:
+			sprite.play("Jump_Down")
+	elif abs(velocity.x) > 10:
+		sprite.play("Run")
+	else:
+		sprite.play("Idle")
+
+func attack() -> void:
+	is_attacking = true
+	sprite.play("attack")
+	await sprite.animation_finished
+	is_attacking = false
+
+func pickup_or_drop() -> void:
+	if held_object:
+		held_object.reparent(get_parent())
+		held_object.position = global_position + Vector2(0, 16)
+		held_object = null
+	else:
+		for body in pickup_area.get_overlapping_bodies():
+			if body.is_in_group("Pickup"):
+				held_object = body
+				held_object.reparent(self)
+				held_object.position = Vector2(0, -16)
+				break
+
+func take_damage() -> void:
+	sprite.play("Hurt")
