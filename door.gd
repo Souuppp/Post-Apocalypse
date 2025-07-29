e67e2e0e9dbcee9baaf0e2d2b3ea3b8d54e79905
Souuppp <levmcg78@kingshigh.school.nz>
2025-07-29 13:26:17 +1200

    yayayay

--- door.gd
+++ door.gd
@@ -1,4 +1,4 @@
 extends CharacterBody2D
 
-func _on_area_2d_area_shape_entered():
+func _on_area_2d_area_entered(area: Area2D) -> void:
 	get_tree().change_scene_to_file("res://Scenes/world_2.tscn")
