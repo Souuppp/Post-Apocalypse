9c968d9a8dadc8b54754f6f801006859d35d19b9
Souuppp <levmcg78@kingshigh.school.nz>
2025-08-14 12:41:43 +1200

    Added Levels

--- Scripts/area_2d.gd
+++ Scripts/area_2d.gd
@@ -2,5 +2,5 @@ extends Area2D
 
 func _on_body_entered(body: Node2D):
 	if body.scene_file_path == "res://Scenes/player.tscn":
-		get_tree().change_scene_to_file("res://Scenes/world_3.tscn")
+		get_tree().change_scene_to_file("res://Scenes/world_2.tscn")
 		
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 const SPEED := 200.0
-const JUMP_FORCE := -340.0
+const JUMP_FORCE := -355.0
 const GRAVITY := 1000.0
 const MAX_JUMPS := 1
 
