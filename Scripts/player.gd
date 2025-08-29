1da5fd0baa14a378047f597ade95198e16e1853a
Souuppp <levmcg78@kingshigh.school.nz>
2025-08-29 12:25:02 +1200

    Pickup shyt

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+class_name Player
 
 const SPEED := 200.0
 const JUMP_FORCE := -355.0
@@ -8,9 +9,24 @@ const MAX_JUMPS := 1
 var jumps_left := MAX_JUMPS
 var is_attacking := false
 var held_object: Node2D = null
+var score := 0
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
-@onready var pickup_area: Area2D = $PickupArea
+@onready var Coin_Sound = $AudioStreamPlayer2D 
+@onready var score_label: RichTextLabel = $Camera2D/RichTextLabel
+
+func _ready():
+	add_to_group("Player")
+	score_label.text = str(score)
+
+func add_score(value):
+	score += 1
+	score_label.text = str(score)
+
+func collect_letter():
+	score += 1
+	score_label.text = str(score)
+	Coin_Sound.play()
 
 func _physics_process(delta: float) -> void:
 	handle_gravity(delta)
@@ -20,22 +36,15 @@ func _physics_process(delta: float) -> void:
 
 func handle_input() -> void:
 	var direction := Input.get_action_strength("right") - Input.get_action_strength("left")
-
 	if not is_attacking:
 		velocity.x = direction * SPEED
-
 		if direction != 0:
 			sprite.flip_h = direction < 0
-
 		if Input.is_action_just_pressed("jump") and jumps_left > 0:
 			velocity.y = JUMP_FORCE
 			jumps_left -= 1
-
 		if Input.is_action_just_pressed("attack"):
 			attack()
-
-		if Input.is_action_just_pressed("pickup"):
-			pickup_or_drop()
 
 	if is_on_floor():
 		jumps_left = MAX_JUMPS
@@ -49,7 +58,6 @@ func handle_gravity(delta: float) -> void:
 func handle_animation() -> void:
 	if is_attacking:
 		return
-
 	if not is_on_floor():
 		if velocity.y < 0:
 			sprite.play("Jump_Up")
@@ -66,18 +74,7 @@ func attack() -> void:
 	await sprite.animation_finished
 	is_attacking = false
 
-func pickup_or_drop() -> void:
-	if held_object:
-		held_object.reparent(get_parent())
-		held_object.position = global_position + Vector2(0, 16)
-		held_object = null
-	else:
-		for body in pickup_area.get_overlapping_bodies():
-			if body.is_in_group("Pickup"):
-				held_object = body
-				held_object.reparent(self)
-				held_object.position = Vector2(0, -16)
-				break
+
 
 func take_damage() -> void:
 	sprite.play("Hurt")
